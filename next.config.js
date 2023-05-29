81f355d523a89bea9d442328b3fb224e2cce1586
aovoq <a21dc163@dhw.ac.jp>
2023-05-30 02:58:32 +0900

    ✨ shader

--- next.config.js
+++ next.config.js
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+   reactStrictMode: false,
+   compiler: {
+      styledComponents: true
+   },
+   webpack: (config) => {
+      config.module.rules.push({
+         test: /\.(glsl|vs|fs|vert|frag)$/,
+         exclude: /node_modules/,
+         use: ['raw-loader', 'glslify-loader']
+      })
+
+      return config
+   }
+}
 
 module.exports = nextConfig
